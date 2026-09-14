341bdbaf2e6933f63782e28b52c31c7279454fb6
maddiekushy <m.e.kusner@gmail.com>
2026-09-14 13:52:43 -0600

    Add files via upload

---
+++ deep_thought.py
@@ -0,0 +1,10 @@
+x = input("What is the answer to life, the universe, and everyhting?")
+
+if x == "42":
+	print("correct")
+
+elif x == "fourty two":
+	print("correct")
+
+else:
+	print("incorrect")
