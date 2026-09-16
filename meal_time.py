2036b75540311dfeb4427c61a5a42b736464218a
maddiekushy <m.e.kusner@gmail.com>
2026-09-16 14:48:02 -0600

    When to eat meals

--- meal_time.py
+++ meal_time.py
@@ -1,19 +1,19 @@
 def meal_time():
     time = input("What time is it?")
 
-    parts = time.split(":")
-    hour = int(parts[0])
-    minute = int(parts[1])
-
-    decimal_hours = hour + (minute / 60)
-
-    if decimal_hours > 6.5 and decimal_hours < 11.5:
-        print("Time for breakfast")
-    if decimal_hours > 11.5 and decimal_hours < 16.5:
-        print("Time for lunch")
-    if decimal_hours > 16.5 and decimal_hours < 22.5:
-        print("Time for dinner")
-    elif decimal_hours > 22.5 and decimal_hours < 6.5:
-        print("Go back to sleep!")
+    def convert(time):
+         parts = time.split(":")
+         hour = int(parts[0])
+         minute = int(parts[1])
+         time = hour + (minute / 60)
+         time = float()
+         if time > 6.5 and time < 11.5:
+            print("Time for breakfast")
+         elif time > 11.5 and time < 16.5:
+            print("Time for lunch")
+         elif time > 16.5 and time < 22.5:
+            print("Time for dinner")
+         elif time > 22.5 and time < 6.5: 
+            print("Go back to sleep!")
 
 meal_time()
