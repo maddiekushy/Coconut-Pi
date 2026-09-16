657e41488d5b1885d164c5f310b86b2629d1a9c7
maddiekushy <m.e.kusner@gmail.com>
2026-09-16 14:24:57 -0600

    Calculate 2-number expressions

---
+++ calc.py
@@ -0,0 +1,17 @@
+expr = input(print("2-number expression:"))
+parts = expr.split()
+
+num1 = float(parts[0])
+op = parts[1]
+num2 = float(parts[2])
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    result = num1 - num2
+elif op == "/":
+    result = num1 / num2
+elif op == "*":
+    result = num1 * num2
+
+print(result)
