4611374c5098e983f4c8734d3a8d458cd99aa7da
maddiekushy <m.e.kusner@gmail.com>
2026-09-18 10:18:01 -0600

    when to eat meals
    Refactor meal_time function and add time conversion.

--- meal_time.py
+++ meal_time.py
@@ -1,19 +1,19 @@
-def meal_time():
-    time = input("What time is it?")
-
-    def convert(time):
+def convert(time):
          parts = time.split(":")
          hour = int(parts[0])
          minute = int(parts[1])
-         time = hour + (minute / 60)
-         time = float()
-         if time > 6.5 and time < 11.5:
-            print("Time for breakfast")
-         elif time > 11.5 and time < 16.5:
-            print("Time for lunch")
-         elif time > 16.5 and time < 22.5:
-            print("Time for dinner")
-         elif time > 22.5 and time < 6.5: 
-            print("Go back to sleep!")
+         return float(hour + (minute / 60))
+    
+def meal_time():
+    time_hm = input("What time is it?")
+    time_decimal = convert(time_hm)
+    if time_decimal > 6.5 and time_decimal < 11.5:
+        print("Time for breakfast")
+    elif time_decimal > 11.5 and time_decimal < 16.5:
+        print("Time for lunch")
+    elif time_decimal > 16.5 and time_decimal < 22.5:
+        print("Time for dinner")
+    else time_decimal 
+        print("Go back to sleep!)
 
 meal_time()
