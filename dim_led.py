80094f6cc499ded8c693538b5e56a8dba567108e
maddiekushy <m.e.kusner@gmail.com>
2026-09-22 14:57:09 -0600

    Update dim_led.py

--- dim_led.py
+++ dim_led.py
@@ -4,14 +4,10 @@ from gpiozero import RotaryEncoder
 
 led = PWMLED(18)
 rotor = RotaryEncoder(16, 20, wrap=True, max_steps=180) # DT goes to 16, CLK goes to 20
+rotor.steps = -180
 
 while True:
-    led.value = 0  # off
-    sleep(1)
-    led.value = 0.5  # half brightness
-    sleep(1)
-    led.value = 1  # full brightness
-    sleep(1)
+    led.value = (rotor.steps + 180) / 360
 
 while True:
 	print(rotor.steps) # the value of rotor.steps will change as you rotate the device
